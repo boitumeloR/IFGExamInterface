9c3d7665303736c8c69c32d999be67912f22caaa
Boitumelo Rampete <u17120323@tuks.co.za>
2021-01-12 22:03:35 +0200

    services

---
+++ src/app/services/global/global.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalService {
+
+  server = 'https://localhost:00000';
+  constructor() { }
+
+  getServer() {
+    return this.server;
+  }
+}
