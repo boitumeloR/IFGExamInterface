f155339b60831901071d24daf7bab5e6aac97701
Boitumelo Rampete <u17120323@tuks.co.za>
2021-01-16 22:02:06 +0200

    reporting

--- src/app/pages/admin-home/admin-home.component.ts
+++ src/app/pages/admin-home/admin-home.component.ts
@@ -1,33 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-admin-home',
   templateUrl: './admin-home.component.html',
   styleUrls: ['./admin-home.component.scss']
 })
-export class AdminHomeComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
+export class AdminHomeComponent implements OnInit {
+  performanceChart!: Chart;
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+  constructor(private breakpointObserver: BreakpointObserver,
+            ) {}
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  ngOnInit(): void {
+  }
+
+  readData(): void {
+
+  }
+
+  GenerateChart(amounts: any[], camps: any[]): void {
+    if (this.performanceChart) {
+      this.performanceChart.destroy();
+    }
+
+    this.performanceChart = new Chart('performanceChart', {
+      type: 'bar',
+      data: {
+        labels: camps,
+        datasets: [
+          {
+            backgroundColor: [
+              'rgb(153,153,255)',
+              'rgb(255,153,238)',
+              'rgb(255,204,179)'
+            ],
+            data: amounts
+          }
+        ]
+      },
+      options: {
+        title: {
+            display: true,
+            text: 'Total Accomodation Revenue Per Camp (R)'
+        },
+        legend: {
+          display: true,
+          labels: {
+              fontColor: 'rgb(255, 99, 132)'
+          }
+        }
+    }
+    });
+  }
 }
